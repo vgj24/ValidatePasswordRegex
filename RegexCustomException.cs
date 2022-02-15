8b193e70bbb2795645c7a8013cbc0c4617e1dad9
vgj24 <vrushlijoshi24@gmail.com>
2022-02-15 15:58:59 +0530

    User Validation with Exception

---
+++ RegexCustomException.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RegexAssgn
+{
+    internal class RegexCustomException : Exception
+    {
+        public enum ExceptionType
+        {
+            NULLVALUE_EXCEPTION,
+            INVALID_FIRSTNAME,
+            INVALID_LASTNAME,
+            INVALID_MOBILENUMBER,
+            INVALID_EMAIL,
+            INVALID_PASSWORD
+        }
+        private readonly ExceptionType type;
+        public RegexCustomException(ExceptionType Type, String message) : base(message)
+        {
+            this.type = Type;
+        }
+    }
+}
